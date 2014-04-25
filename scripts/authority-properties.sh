959f8f107511922083508ed139bbad4bd2d10be2
Jakob Voss <voss@gbv.de>
2014-04-25 23:01:48 +0200

    script to collect all authority properties

--- scripts/authority-properties.sh
+++ scripts/authority-properties.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-PROPERTIES=`awk -v ORS=',' '{print $1}' authority-properties.csv`
-
-FIX='copy_field("labels.de.value","x.name"); copy_field("id","x.id"); retain_field("x"); move_field("x.id","id"); move_field("x.name","name"); remove_field("x")'
-
-catmandu convert Wikidata --ids "$PROPERTIES" --fix "$FIX" to JSON 
-
-# TODO: just use template to convert to CSV (name, description, id)
-#catmandu convert Wikidata --ids "$PROPERTIES" --fix "$FIX" to Template --template json2csv.tt
-
 # TODO: see https://www.wikidata.org/wiki/MediaWiki:Gadget-AuthorityControl.js
 # see also <http://meta.wikimedia.org/wiki/Reasonator/stringprops>
 # TODO: get Wikidata, item, retain authority properties, create mapping / search for literature
 # TODO: add literature link to each authority property in Wikidata
+
+# - get the time the property was first introduced
+# - get the number of items that use this property
+# - get URI pattern
